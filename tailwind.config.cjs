7b8c42ab9d8e5249e8138ac2cd2e9d440fbaea52
lucas <lucasfrutig0@gmal.com>
2023-09-21 16:46:57 -0300

    feature: add route and view transition

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,13 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      backgroundImage: {
+        'bg-teste': "url('../public/img/testebg1.png')"
+      },
       colors: {
         light: '#F8F8F8',
-        dark: '#023047',
+        dark: '#111827',
+        bluedark: '#023047',
         skyblue: '#8ECAE6',
         bluegreen: '#219EBC',
         yellow: '#FFB703',
